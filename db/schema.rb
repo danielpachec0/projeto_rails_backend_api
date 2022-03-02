50b502e265d1537613e37acea4856ff3481bd4da
= <=>
2022-03-02 09:42:24 -0300

    Created visit model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_02_031806) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_02_123743) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -19,4 +19,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_02_031806) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "visits", force: :cascade do |t|
+    t.datetime "date"
+    t.string "status"
+    t.datetime "checkin_at"
+    t.datetime "checkout_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_visits_on_user_id"
+  end
+
+  add_foreign_key "visits", "users"
 end
---
+++ spec/factories/visits.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :visit do
+    date { "2022-03-02 09:33:17" }
+    status { "MyString" }
+    checkin_at { "2022-03-02 09:33:17" }
+    checkout_at { "2022-03-02 09:33:17" }
+  end
+end
