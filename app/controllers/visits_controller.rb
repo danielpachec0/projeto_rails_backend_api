c5e976a91fc40337aca6d4ce09a73063b740cbc6
= <=>
2022-03-02 13:07:46 -0300

    added index and create methods in visits controller and the tests in its spec file

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -1,2 +1,28 @@
 class VisitsController < ApplicationController
+    def index
+        @visits = Visit.all
+
+        render json: @visits
+    end 
+
+    def create
+        @visit = Visit.new(visit_params)
+    
+        if @visit.save
+          render json: @visit, status: :created, location: @visit
+        else
+          render json: @visit.errors, status: :unprocessable_entity
+        end
+    end
+
+    private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_visit
+      @visit = Visit.find(params[:id])
+    end
+
+    def visit_params
+        params.require(:visit).permit(:date, :status, :user_id,:checkin_at, :checkout_at)
+    end
+
 end
--- spec/requests/visits_spec.rb
+++ spec/requests/visits_spec.rb
@@ -1,7 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe "Visits", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe 'GET /visits' do
+    before do
+      create(:visit)
+      create(:visit, user_id: 1)
+    end
+    it 'return all visits' do
+      get visits_url
+
+      expect(response).to have_http_status(:success)
+      expect(JSON.parse(response.body).size).to eq(2)
+    end
+  end
+
+  describe 'POST /visits' do
+    before do
+      create(:user)
+    end
+    it 'Creates a new visit' do
+      
+      expect {
+        post visits_url, params: { visit: { date: DateTime.current, status: 'status', user_id: 1, checkin_at: DateTime.yesterday,  checkout_at:  DateTime.tomorrow.tomorrow} }
+      }.to change(Visit, :count).from(0).to(1)
+
+
+      expect(response).to have_http_status(:created)
+    end
   end
 end
