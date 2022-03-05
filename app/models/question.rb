5810aa75ad1c3bb5c7484f9241635fa78d2b18bf
= <=>
2022-03-05 00:08:07 -0300

    installed active storage and added an attached image to question model

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,8 @@
 class Question < ApplicationRecord
     validates :name, uniqueness: {  scope: :formulary_id,
                                     message: "uniquennes msg placeholder"}
+    
+    has_one_attached :image
 
     belongs_to :formulary
     has_one :answer
