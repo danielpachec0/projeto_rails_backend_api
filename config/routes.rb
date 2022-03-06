6d147e7073ab65602ca3489efcf221cd12b34e22
Daniel <dap5@cin.ufpe.br>
2022-03-06 01:41:04 -0300

    Created authenticate controller to manage authentication requests and added its route

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :formularies
   resources :users
   resources :visits
+  post 'authenticate', to: 'authentication#authenticate'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
