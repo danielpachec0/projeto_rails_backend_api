1a1edb9fbd575cc751b11ce6d1e2bf4810c25d10
Daniel <dap5@cin.ufpe.br>
2022-03-06 03:56:28 -0300

    added authentication request method to ApplicationController and useded it as attribute in before_action to add token based authentication to the requests and updated the requests specs to use authentication

--- spec/requests/answers_spec.rb
+++ spec/requests/answers_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 
 
 RSpec.describe "Answers api", type: :request do
+  let!(:user) { create(:user) }
+  let!(:token) { get_token(user) }
+
   describe 'GET /answers' do
     before do
       form = create(:formulary)
@@ -10,7 +13,7 @@ RSpec.describe "Answers api", type: :request do
       create(:answer, question: question, formulary: form)
     end
     it 'return all answers' do
-      get answers_url
+      get answers_url, headers: { Authorization: token }
       
       expect(response).to have_http_status(:success)
       expect(JSON.parse(response.body).size).to eq(2)
@@ -21,7 +24,7 @@ RSpec.describe "Answers api", type: :request do
 
     let!(:answer) { create(:answer, formulary: create(:formulary), question: create(:question, :text, formulary_id: 1)) } 
     it 'return the specified formulary' do
-      get formulary_url(answer.id)
+      get formulary_url(answer.id), headers: { Authorization: token }
       
       expect(response).to have_http_status(:success)
     end
@@ -34,7 +37,9 @@ RSpec.describe "Answers api", type: :request do
     end
     it 'create a new answers' do
       expect {
-        post answers_url, params: { answer: { content: 'qustion content', formulary_id: 1, question_id: '1' } }
+        post answers_url,
+        params: { answer: { content: 'qustion content', formulary_id: 1, question_id: '1' } },
+        headers: { Authorization: token }
       }.to change(Answer, :count).from(0).to(1)
       
 
@@ -46,10 +51,11 @@ RSpec.describe "Answers api", type: :request do
   describe 'PATCH /answers/:id' do
     let!(:answer) { create(:answer, formulary: create(:formulary), question: create(:question, :text, formulary_id: 1)) } 
  
-
     it 'updates a answer' do
       expect {
-        patch answer_url(answer.id), params: { answer: { content: 'new content' } }  
+        patch answer_url(answer.id),
+        params: { answer: { content: 'new content' } },
+        headers: { Authorization: token }
       }.to_not change(Answer, :count)
 
       expect(response).to have_http_status(:ok)
@@ -61,10 +67,17 @@ RSpec.describe "Answers api", type: :request do
 
     it 'deletes a answer' do
       expect {
-        delete answer_url(answer.id) 
+        delete answer_url(answer.id), headers: { Authorization: token } 
       }.to change(Answer, :count).from(1).to(0)
 
       expect(response).to have_http_status(:no_content)
     end
   end 
+end
+
+def get_token(user)
+  post '/authenticate', params: { email: user.email, password: user.password}
+  token = JSON.parse(response.body)
+  t =   token["auth_token"] 
+  return t
 end
