52a555bc74fe14f7d3622e1cd3cbcaaa449e4c20
Daniel <dap5@cin.ufpe.br>
2022-03-08 20:50:01 -0300

    updated visit factory

--- spec/factories/visits.rb
+++ spec/factories/visits.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :visit do
     date { DateTime.current }
     status { "realizado" }
-    user
+    user factory: :user
     checkin_at { DateTime.yesterday }
     checkout_at { DateTime.tomorrow.tomorrow }
   end
