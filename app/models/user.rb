4ba09045dc5ee00a58aa91db05640ba4f70a9396
Daniel <dap5@cin.ufpe.br>
2022-03-09 02:26:20 -0300

    formated user model check_cpf function for betther readability

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
     validates :cpf, format: { with: VALID_CPF_REGEX }
     validates :email, format: { with: VALID_EMAIL_REGEX }
     validates :email, uniqueness: true
-    validate :check_cpf
+    validate :check_cpf, unless: -> { cpf.nil?  }
     validates :cpf, uniqueness: true
     validates :password, length: { minimum: 6 } 
     validates :password, format: { with: VALID_PASSWORD_REGEX }
@@ -20,50 +20,47 @@ class User < ApplicationRecord
     private
       
     def check_cpf
-        if cpf.nil? 
-            errors.add(:cpf, "is not valid")
-            return
-        else
-            cpf_mod = cpf.tr('.', '')
-            cpf_mod = cpf_mod.tr('-', '')
-        end
+
+        cpf_mod = cpf.tr('.', '')
+        cpf_mod = cpf_mod.tr('-', '')
+
         if cpf_mod.length != 11 
-            errors.add(:cpf, "is not valid")
+            errors.add(:cpf, "does not have the correct lenght")
             return
         end
 
-        equal = true
+        #check if all the numbers are the same
+        all_numbers_equal = true
         (1..10).step do |i|
             if cpf_mod[i] != cpf_mod[i-1]
-                equal =  false
+                all_numbers_equal =  false
                 break
             end
         end
-    
-        if equal == true 
-             errors.add(:cpf, "invalid cpf")
+        if all_numbers_equal
+             errors.add(:cpf, "is not valid")
              return
         end
     
-        y = []
+        number_array = []
     
         cpf_mod.each_char do |char| 
-            y.push char.to_i 
+            number_array.push char.to_i 
         end 
     
         sum1 = 0
         sum2 = 0
         (0..9).step do |i|
             unless i == 9
-                sum1 = sum1 + (y[i] * (10-i))
+                sum1 = sum1 + (number_array[i] * (10-i))
             end
-            sum2 = sum2 + (y[i] * (11-i))
+            sum2 = sum2 + (number_array[i] * (11-i))
         end 
         mod1 = (sum1 * 10)%11
         if mod1 == 10 then mod1 = 0 end
         mod2 = (sum2 * 10)%11
         if mod2 == 10 then mod2 = 0 end
-        if !(mod1 == y[9] && mod2 == y[10])
+        if !(mod1 == number_array[9] && mod2 == number_array[10])
             errors.add(:cpf, "is not valid")
             return
         end
